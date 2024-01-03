b1b334e006eaec66ffbc6485c977dfc326c28289
Pósán Róbert <posanrobi@gmail.com>
2024-01-03 19:36:02 +0100

    create starter files for backend and frontend

---
+++ backend/BusReservationSystem/src/main/java/com/thesispr/BusReservationSystem/BusReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thesispr.BusReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationSystemApplication.class, args);
+	}
+
+}
---
+++ backend/BusReservationSystem/src/test/java/com/thesispr/BusReservationSystem/BusReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesispr.BusReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

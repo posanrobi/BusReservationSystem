ff9e448392b2fbfbc48128b16802d597f7d18492
Pósán Róbert <posanrobi@gmail.com>
2024-01-04 17:31:43 +0100

    add entities

---
+++ backend/BusReservationSystem/src/main/java/com/thesispr/BusReservationSystem/model/BusLineDate.java
@@ -0,0 +1,48 @@
+package com.thesispr.BusReservationSystem.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class BusLineDate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "bus_line_id")
+    private BusLine busLine;
+
+    public BusLineDate() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BusLine getBusLine() {
+        return busLine;
+    }
+
+    public void setBusLine(BusLine busLine) {
+        this.busLine = busLine;
+    }
+}
+

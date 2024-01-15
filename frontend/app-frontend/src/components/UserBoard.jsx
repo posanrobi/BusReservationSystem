8b74a1a562dc67872c3de73b11a814e46c2db27d
Pósán Róbert <posanrobi@gmail.com>
2024-01-15 02:00:52 +0100

    homepage first verison done

--- frontend/app-frontend/src/components/UserBoard.jsx
+++ frontend/app-frontend/src/components/UserBoard.jsx
@@ -5,8 +5,13 @@ import classes from "./UserBoard.module.css";
 export default function HomePage() {
   return (
     <>
-      <div className={classes.backGround}>
-        <div className={classes.homeContainer}>
+      <div className={classes.homeContainer}>
+        <h1 className={classes.homeTitle}>Bookerra</h1>
+        <p className={classes.homeDescription}>
+          Welcome to dolor sit amet, consectetur adipisicing elit. Tempore
+          corporis optio modi commodi assumenda quibusdam aspernatur!
+        </p>
+        <div className={classes.navBoxContainer}>
           <div className={classes.navBox}>
             <Link to={"/plan"}>Planning</Link>
           </div>

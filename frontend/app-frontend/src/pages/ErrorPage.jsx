6c25d2e699866847a01bce6c06c10aef77fbb4b0
Pósán Róbert <posanrobi@gmail.com>
2024-01-29 01:37:22 +0100

    errorPage styling

--- frontend/app-frontend/src/pages/ErrorPage.jsx
+++ frontend/app-frontend/src/pages/ErrorPage.jsx
@@ -1,15 +1,31 @@
-import { Link, redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { getUserRole } from "../services/auth.service";
+import { FaArrowLeft } from "react-icons/fa";
+
+import classes from "./ErrorPage.module.css";
 
 export default function Error() {
   const isAdmin = getUserRole() === "ROLE_ADMIN";
 
   return (
     <>
-      <h1>An error occured!</h1>
-      <p>Wrong path detected.</p>
+      <div className={classes.errorContainer}>
+        <div className={classes.errorBox}>
+          <h1 className={classes.errorTitle}>An error occured!</h1>
+          <h2>:(</h2>
+          <p>Wrong path detected.</p>
 
-      <Link to={`${isAdmin ? "/admin" : "/home"}`}>Back to content</Link>
+          <Link
+            to={`${isAdmin ? "/admin" : "/home"}`}
+            className={classes.backToButton}
+          >
+            <div className={classes.backToDiv}>
+              <FaArrowLeft />
+              Back to content
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }

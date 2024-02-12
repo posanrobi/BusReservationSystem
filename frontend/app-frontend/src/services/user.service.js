917bfd365e089efb1518116192d5aef0cb32e5c5
Pósán Róbert <posanrobi@gmail.com>
2024-02-12 18:58:27 +0100

    document a service code in frontend

--- frontend/app-frontend/src/services/user.service.js
+++ frontend/app-frontend/src/services/user.service.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
+/**
+ * The base URL of the API server.
+ */
 const API_URL = "http://localhost:8080/api/";
 
-// get all users
+/**
+ * Fetches all users from the server.
+ *
+ * @returns {Promise<Object>} A Promise containing the response data with all users.
+ * @throws {Error} If the request fails due to unauthorized access or expired token.
+ */
 export async function getAllUsers() {
   try {
     return await axios.get(API_URL + "users", { headers: authHeader() });
@@ -15,7 +23,12 @@ export async function getAllUsers() {
   }
 }
 
-// get all buslines
+/**
+ * Fetches all buslines from the server.
+ *
+ * @returns {Promise<Object>} A Promise containing the response data with all buslines.
+ * @throws {Error} If the request fails due to unauthorized access or expired token.
+ */
 export async function getAllBusLines() {
   try {
     return await axios.get(API_URL + "bus-lines", { headers: authHeader() });
@@ -27,7 +40,12 @@ export async function getAllBusLines() {
   }
 }
 
-// get all dates and times
+/**
+ * Fetches all busline dates and times from the server.
+ *
+ * @returns {Promise<Object>} A Promise containing the response data with the dates and times.
+ * @throws {Error} If the request fails due to unauthorized access or expired token.
+ */
 export async function getAllBusLineDatesAndTimes() {
   try {
     return await axios.get(API_URL + "busline-date-time", {
@@ -41,7 +59,12 @@ export async function getAllBusLineDatesAndTimes() {
   }
 }
 
-// get all reservations
+/**
+ * Fetches all reservation data from the server.
+ *
+ * @returns {Promise<Object>} A Promise containing the response data with all reservations.
+ * @throws {Error} If the request fails due to unauthorized access or expired token.
+ */
 export async function getAllReservations() {
   try {
     return await axios.get(API_URL + "reservations", {
@@ -55,7 +78,13 @@ export async function getAllReservations() {
   }
 }
 
-// reservation data
+/**
+ * Creates a new reservation on the server.
+ *
+ * @param {Object} reservationData - The data for the reservation to be created.
+ * @returns {Promise<Object>} A Promise containing the response data from the server.
+ * @throws {Error} If the request fails or if the user's token is expired.
+ */
 export async function createReservation(reservationData) {
   const response = await axios.post(API_URL + "reservations", reservationData, {
     headers: authHeader(),
@@ -63,7 +92,13 @@ export async function createReservation(reservationData) {
   return response;
 }
 
-// delete reservation by id
+/**
+ * Deletes a reservation from the server by its ID.
+ *
+ * @param {string} resId - The ID of the reservation to be deleted.
+ * @returns {Promise} A Promise indicating the success of the deletion.
+ * @throws {Error} If the request fails or if there is an issue with the deletion.
+ */
 export async function deleteReservation(resId) {
   try {
     return await axios.delete(API_URL + "reservations/" + resId, {
@@ -74,7 +109,13 @@ export async function deleteReservation(resId) {
   }
 }
 
-// get user by id
+/**
+ * Fetches a user by its ID.
+ *
+ * @param {String} userId The ID of the user to be fetched.
+ * @returns {Promise<Object>} A Promise containing the response data from the server.
+ * @throws {Error} If the request fails or if the user's token is expired.
+ */
 export async function getUserById(userId) {
   try {
     return await axios.get(API_URL + "users/" + userId, {
@@ -88,7 +129,14 @@ export async function getUserById(userId) {
   }
 }
 
-// update password
+/**
+ * Updates the password of a user by their ID.
+ *
+ * @param {String} userId The ID of the user whose password will be updated.
+ * @param {Object} passwordData The new password data.
+ * @returns {Promise} A Promise representing the result of the password update request.
+ * @throws {Error} If the request fails or if there is an issue updating the password.
+ */
 export async function updatePassword(userId, passwordData) {
   try {
     return await axios.put(
@@ -105,7 +153,14 @@ export async function updatePassword(userId, passwordData) {
   }
 }
 
-// update user (firstname, lastname, username, email)
+/**
+ * Upate user data with the given user ID. Includes firsname, lastname, username, email.
+ *
+ * @param {String} userId The ID of the user whose data will be updated.
+ * @param {*} userData The new user data.
+ * @returns {Promise<Object>} A Promise representing the result of the user update request.
+ * @throws {Error} If the request fails.
+ */
 export async function updateUser(userId, userData) {
   try {
     return await axios.put(API_URL + "users/" + userId, userData, {
@@ -117,7 +172,13 @@ export async function updateUser(userId, userData) {
   }
 }
 
-// delete user by id
+/**
+ * Deletes a user by its ID.
+ *
+ * @param {string} userId - The ID of the user to be deleted.
+ * @returns {Promise} A Promise representing the result of the deletion request.
+ * @throws {Error} If the request fails.
+ */
 export async function deleteUser(userId) {
   try {
     return await axios.delete(API_URL + "users/" + userId, {

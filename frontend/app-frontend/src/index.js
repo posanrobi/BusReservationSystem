2d1aad688a770de8059faaf523ed1d59106ba313
Pósán Róbert <posanrobi@gmail.com>
2024-02-13 17:42:54 +0100

    outsourcing font family

--- frontend/app-frontend/src/index.js
+++ frontend/app-frontend/src/index.js
@@ -5,6 +5,12 @@ import "./index.css";
 
 import App from "./App";
 
+/**
+ * Entry point of the React application.
+ * Initializes the root element and renders the main <App /> component.
+ * Utilizes React.StrictMode for development mode checks and optimizations.
+ * @module Index
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

9abf044afabbf345e849af629772191f55c7aeb2
Pósán Róbert <posanrobi@gmail.com>
2024-02-27 17:38:48 +0100

    authController unit tests passed

--- backend/BusReservationSystem/src/test/java/com/thesispr/BusReservationSystem/controller/AuthControllerTest.java
+++ backend/BusReservationSystem/src/test/java/com/thesispr/BusReservationSystem/controller/AuthControllerTest.java
@@ -1,7 +1,7 @@
 package com.thesispr.BusReservationSystem.controller;
 
+import com.thesispr.BusReservationSystem.model.ERole;
 import com.thesispr.BusReservationSystem.model.Role;
-import com.thesispr.BusReservationSystem.model.User;
 import com.thesispr.BusReservationSystem.payload.request.LoginRequest;
 import com.thesispr.BusReservationSystem.payload.request.SignupRequest;
 import com.thesispr.BusReservationSystem.payload.response.JwtResponse;
@@ -10,24 +10,22 @@ import com.thesispr.BusReservationSystem.repository.RoleRepository;
 import com.thesispr.BusReservationSystem.repository.UserRepository;
 import com.thesispr.BusReservationSystem.security.jwt.JwtUtils;
 import com.thesispr.BusReservationSystem.security.services.UserDetailsImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 public class AuthControllerTest {
 
     @InjectMocks
@@ -48,8 +46,13 @@ public class AuthControllerTest {
     @Mock
     private JwtUtils jwtUtils;
 
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void testAuthenticateUser() {
+    public void testAuthenticateUser() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("testuser");
         loginRequest.setPassword("testpassword");
@@ -57,7 +60,7 @@ public class AuthControllerTest {
         Authentication authentication = mock(Authentication.class);
         when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
 
-        UserDetailsImpl userDetails = new UserDetailsImpl(1L, "testuser", "test@test.com", "password", Collections.emptyList());
+        UserDetailsImpl userDetails = new UserDetailsImpl(1L, "testuser", "test@test.com", "password", "John","Doe", Collections.emptyList());
         when(authentication.getPrincipal()).thenReturn(userDetails);
 
         when(jwtUtils.generateJwtToken(authentication)).thenReturn("testjwt");
@@ -65,25 +68,28 @@ public class AuthControllerTest {
         ResponseEntity<?> responseEntity = authController.authenticateUser(loginRequest);
 
         assertEquals(200, responseEntity.getStatusCodeValue());
-        assertEquals(JwtResponse.class, responseEntity.getBody().getClass());
+        assertEquals(JwtResponse.class, Objects.requireNonNull(responseEntity.getBody()).getClass());
     }
 
     @Test
-    void testRegisterUser() {
+    public void testRegisterUser() {
         SignupRequest signupRequest = new SignupRequest();
         signupRequest.setUsername("testuser");
         signupRequest.setEmail("test@test.com");
-        signupRequest.setPassword("testpassword");
-        signupRequest.setFirstname("Test");
-        signupRequest.setLastname("User");
-        signupRequest.setRole(Collections.singletonList("ROLE_USER"));
+        signupRequest.setPassword("password");
+        signupRequest.setFirstname("John");
+        signupRequest.setLastname("Doe");
+
+        Set<String> roles = new HashSet<>();
+        roles.add("ROLE_USER");
+        signupRequest.setRole(roles);
 
         when(userRepository.existsByUsername("testuser")).thenReturn(false);
         when(userRepository.existsByEmail("test@test.com")).thenReturn(false);
 
         Role role = new Role();
         role.setId(1L);
-        role.setRoleName("ROLE_USER");
+        role.setRoleName(ERole.valueOf("ROLE_USER"));
         when(roleRepository.findByRoleName(any())).thenReturn(Optional.of(role));
 
         when(encoder.encode("testpassword")).thenReturn("encodedpassword");
@@ -91,6 +97,7 @@ public class AuthControllerTest {
         ResponseEntity<?> responseEntity = authController.registerUser(signupRequest);
 
         assertEquals(200, responseEntity.getStatusCodeValue());
-        assertEquals(MessageResponse.class, responseEntity.getBody().getClass());
+        assertEquals(MessageResponse.class, Objects.requireNonNull(responseEntity.getBody()).getClass());
     }
+
 }
